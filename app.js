9a5acb5d0c37827f12ae165bb111573f44c3c9a9
ShikharSingh98 <shikharsingh98@gmail.com>
2021-08-26 01:50:34 +0530

    Created and implemented modal functionality

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const movieContainer = document.getElementById('movies-container');
 const loader = document.getElementById('loader');
+const modal = document.getElementById('modal');
+const closeModal = document.getElementById('close-modal');
+const trailerVideo = document.getElementById('trailer-video');
+const movieName = document.getElementById('movie-name');
+const rating = document.getElementById('rating');
+const overview = document.getElementById('overview');
 
 const baseURL = `https://api.themoviedb.org/3/movie`;
 
 let movies = [];
+
+let selectedMovie = null;
+
+async function getDataFromApi(additionalUrl) {
+  loading();
+  const response = await fetch(`${baseURL}${additionalUrl}`, {
+    headers: {
+      authorization: `Bearer ${API_KEY}`,
+    },
+  });
+  const data = await response.json();
+  if (data.results) return data.results;
+  return data;
+}
 
 function loading() {
   movieContainer.hidden = true;
@@ -15,33 +35,45 @@ function completed() {
   loader.hidden = true;
 }
 
+closeModal.addEventListener('click', function () {
+  modal.classList.remove('show-modal');
+});
+
+async function openModal(event) {
+  modal.classList.add('show-modal');
+  selectedMovie = await getDataFromApi(
+    `/${event.target.dataset.movieId}?append_to_response=videos`
+  );
+  if (selectedMovie.videos.results.length) {
+    const movieTrailerVideo = selectedMovie.videos.results.find(
+      (movie) => movie.type === 'Trailer' || movie.type === 'Teaser'
+    );
+    trailerVideo.setAttribute(
+      'src',
+      `https://www.youtube.com/embed/${movieTrailerVideo.key}`
+    );
+  }
+  movieName.textContent = selectedMovie.title;
+  rating.textContent = selectedMovie.vote_average;
+  overview.textContent = selectedMovie.overview;
+}
+
 function createMovieCard(movie) {
   const { id, poster_path } = movie;
 
   const img = document.createElement('img');
   img.setAttribute('src', `https://image.tmdb.org/t/p/w500${poster_path}`);
   img.dataset.movieId = id;
-
+  img.addEventListener('click', openModal);
   movieContainer.append(img);
 }
 
-function displayMovieCard() {
+async function displayMovieCard() {
+  movies = await getDataFromApi('/popular?language=en-US&page=1');
   movies.forEach((movie) => {
     createMovieCard(movie);
   });
   completed();
 }
 
-async function getAllPopularMovies() {
-  loading();
-  const response = await fetch(`${baseURL}/popular?language=en-US&page=1`, {
-    headers: {
-      authorization: `Bearer ${API_KEY}`,
-    },
-  });
-  const data = await response.json();
-  movies = data.results;
-  displayMovieCard();
-}
-
-getAllPopularMovies();
+displayMovieCard();
